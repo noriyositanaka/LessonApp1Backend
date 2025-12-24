e900b99ae24261facc33e3c20d8b7084659ac264
noriyoshi.tanaka <aburagirio@gmail.com>
2025-12-25 07:52:54 +0900

    Json形式でレスポンスするように修正した

--- src/main/java/gcfv2/HelloHttpFunction.java
+++ src/main/java/gcfv2/HelloHttpFunction.java
@@ -5,10 +5,20 @@ import java.io.BufferedWriter;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+import com.google.gson.Gson;
 
 public class HelloHttpFunction implements HttpFunction {
-  public void service(final HttpRequest request, final HttpResponse response) throws Exception {
+ 
+    static class ResponseMessage {
+    String message;
+    ResponseMessage(String message) { this.message = message; }
+  }
+
+ private static final Gson gson = new Gson();
+
+   public void service(final HttpRequest request, final HttpResponse response) throws Exception {
+    response.appendHeader("Content-Type", "application/json");
     final BufferedWriter writer = response.getWriter();
-    writer.write("Hello world!");
+    writer.write(gson.toJson(new ResponseMessage("Hello world!")));
   }
 }
